94fd3a5766fd49c4f7b8d889e1f75289f450d4ea
Xie Yinuo <136900616+EnochxynL@users.noreply.github.com>
2025-08-10 12:36:53 +0800

    代码美化

--- User_Ctrl/src/run.cpp
+++ User_Ctrl/src/run.cpp
@@ -1,5 +1,5 @@
 #include "run.h"
-
+#include <rtthread.h>
 extern "C" {
 #include "lcd_init.h"
 #include "lcd.h"
@@ -7,17 +7,15 @@ extern "C" {
 
 void setup() {
   // put your setup code here, to run once:
-
 	LCD_Init();//LCDLCD初始化
 	LCD_Fill(0,0,LCD_W,LCD_H,WHITE);	
 //	LCD_ShowPicture(20,45,120,29,gImage_pic1);
 	LCD_ShowString(50,0,(std::uint8_t*)("CIRNO!!!"),BLACK,WHITE,16,0);
 	LCD_ShowChinese(50,20,(std::uint8_t*)("无名科技"),BLACK,WHITE,16,0);	
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  rt_thread_mdelay(1000);
-//  rt_kprintf("\nHello RT-Thread \n");
+  rt_thread_mdelay(1000);
+  rt_kprintf("\nHello RT-Thread \n");
 }
