27c356d8130f4f40d8be460701bfdcee508be27f
Xie Yinuo <136900616+EnochxynL@users.noreply.github.com>
2025-09-15 15:28:04 +0800

    PlatformIO成功编译

--- User_Ctrl/src/start.cpp
+++ User_Ctrl/src/start.cpp
@@ -10,8 +10,8 @@ void setup() {
 	LCD_Init();//LCDLCD初始化
 	LCD_Fill(0,0,LCD_W,LCD_H,WHITE);	
 //	LCD_ShowPicture(20,45,120,29,gImage_pic1);
-	LCD_ShowString(50,0,(std::uint8_t*)("CIRNO!!!"),BLACK,WHITE,16,0);
-	LCD_ShowChinese(50,20,(std::uint8_t*)("无名科技"),BLACK,WHITE,16,0);	
+	LCD_ShowString(50,0,(uint8_t*)("CIRNO!!!"),BLACK,WHITE,16,0);
+	LCD_ShowChinese(50,20,(uint8_t*)("无名科技"),BLACK,WHITE,16,0);	
 }
 
 void loop() {
